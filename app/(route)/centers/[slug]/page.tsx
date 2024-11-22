e294a117350cb899561efc1eae1f713778c0765f
kenjanjan <kjliloc@hcdc.edu.ph>
2024-11-22 08:02:30 +0800

    remove comment

--- app/(route)/centers/[slug]/page.tsx
+++ app/(route)/centers/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CenterBody from "@/components/CenterBody";
 
-// The 'params' prop is automatically passed by Next.js for dynamic routes
 export default function CenterView() {
   return (
     <>
