8d62e5e4879f48d0ac0b8a192a15917e6a9de9c0
kenjanjan <kjliloc@hcdc.edu.ph>
2024-11-26 01:30:42 +0800

    url not found if wrong

--- app/(route)/location/[region]/page.tsx
+++ app/(route)/location/[region]/page.tsx
@@ -1,12 +1,29 @@
+import { notFound } from "next/navigation";
 import RegionBody from "@/components/RegionBody";
 import React from "react";
 
-const RegionPage = () => {
+async function fetchRegions() {
+  return [{ region: "PA" }, { region: "VA" }, { region: "WV" }];
+}
+
+export default async function RegionPage({
+  params,
+}: {
+  params: { region: string };
+}) {
+  const { region } = params;
+
+  const dataset = await fetchRegions();
+
+  const regionData = dataset.find((item) => item.region === region);
+
+  if (!regionData) {
+    notFound();
+  }
+
   return (
     <>
       <RegionBody />
     </>
   );
-};
-
-export default RegionPage;
+}
